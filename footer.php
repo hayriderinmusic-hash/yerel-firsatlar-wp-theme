e8c62da708a15c130fe21a50838b882651a28739
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 01:40:27 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer style="background:#222;color:#fff;padding:20px;text-align:center;">
+    © <?php echo date('Y'); ?> Yerel Fırsatlar
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
