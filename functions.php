ac8c18c85744fa8f6226cc7eae36aca416e8673f
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 01:57:48 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,5 @@
 <?php
-if (!defined('ABSPATH')) exit;
-
-function yerel_firsatlar_setup() {
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-}
-add_action('after_setup_theme', 'yerel_firsatlar_setup');
-
 function yerel_firsatlar_assets() {
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+  wp_enqueue_style('style', get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts', 'yerel_firsatlar_assets');
+add_action('wp_enqueue_scripts','yerel_firsatlar_assets');
