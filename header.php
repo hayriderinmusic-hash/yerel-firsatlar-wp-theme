aeb029d99ed041b87c63dfe5d613188b789bae62
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 01:40:05 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header style="background:#ff7a00;color:#fff;padding:15px;">
+    <div class="container">
+        <h2>Yerel Fırsatlar</h2>
+    </div>
+</header>
+<div class="container">
