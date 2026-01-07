8a0c995828f65a037f398b40481fed58ffee49e9
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 02:48:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-get_header();
-?>
-
-<div class="container">
-  <h2>Yerel Fırsatlar Tema Test</h2>
-  <p>Eğer bunu görüyorsan tema çalışıyor.</p>
-</div>
-
-<?php
-get_footer();
+// Silence is golden.
