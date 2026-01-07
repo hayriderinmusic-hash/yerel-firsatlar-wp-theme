0e67f0fa0e5a26b884da506f5c5373af50d712f1
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 02:08:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php get_header(); ?>
+<?php
+get_header();
+?>
 
-<h1>Güncel Fırsatlar</h1>
+<div class="container">
+  <h2>Yerel Fırsatlar Tema Test</h2>
+  <p>Eğer bunu görüyorsan tema çalışıyor.</p>
+</div>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <article style="background:#fff;padding:15px;margin-bottom:15px;">
-        <h2><?php the_title(); ?></h2>
-        <?php the_excerpt(); ?>
-        <a href="<?php the_permalink(); ?>">Detayı Gör</a>
-    </article>
-<?php endwhile; else : ?>
-    <p>Henüz ilan yok.</p>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+<?php
+get_footer();
