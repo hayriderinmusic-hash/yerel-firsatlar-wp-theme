cf57d5b53efc22f27251678985caa743d1011fc3
hayriderinmusic-hash <hayriderinmusic@gmail.com>
2026-01-08 01:57:28 +0300

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+  <div class="vitrin">
+    VİTRİN
+  </div>
+
+  <h3>ÇARŞI’DA İNDİRİM ARAMA, İNDİRİMLER BURADA!</h3>
+
+  <div class="kategoriler">
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/food.svg">
+      Yemek
+    </div>
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/clothes.svg">
+      Giyim
+    </div>
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/glasses.svg">
+      Aksesuar
+    </div>
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/car.svg">
+      Oto
+    </div>
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/heart.svg">
+      Sağlık
+    </div>
+    <div class="kategori">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/settings.svg">
+      Hizmet
+    </div>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
